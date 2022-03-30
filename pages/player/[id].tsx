19346aa66e58023fdfc74f1e3a63e66edc77efc0
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-30 18:36:21 -0400

    update

--- pages/player/[id].tsx
+++ pages/player/[id].tsx
@@ -32,6 +32,8 @@ const Player = () => {
         <meta name="description" content="lorem ipsum" />
       </Head>
       {/* Some more stats I can add here just rough layout for now */}
+      {/* Match History section either set value of 24rem or max-content dependant on content */}
+      {/* Spacing / Gap can either be through flexbox (right now it is flexbox) can be center with gap-x */}
       <div className="flex justify-evenly mt-32">
         <div className="flex-none w-max bg-red-50">
           No idea what to add here
@@ -39,7 +41,7 @@ const Player = () => {
         <div className="flex-initial w-96 bg-gray-50">
           Matches / Games Played Section
         </div>
-        <div className="flex-initial w-max bg-slate-400 rounded-lg">
+        <div className="flex-initial w-max bg-slate-100 rounded-lg">
           <div className="flex justify-center gap-x-6 p-4 items-center">
             <h1 className="font-medium">{player.name}</h1>
             <img
@@ -48,7 +50,39 @@ const Player = () => {
               className="h-12 rounded-md"
             />
           </div>
+          <div className="flex-col">
+            <span className="flex p-4 my-4 justify-start items-center text-lg font-medium ">
+              <img
+                className="w-8"
+                src={player.ratings[0].rankIcon}
+                alt="skill rating icon"
+              />
+              <h3 className=''>{player.ratings[0].level}</h3>
+            </span>
+            <span className="flex p-4 my-4 justify-start items-center text-lg">
+              <img
+                className="w-8"
+                src={player.ratings[0].roleIcon}
+                alt="skill rating icon"
+              />
+              <h3 className=''>
+                {player.ratings[0].role.charAt(0).toUpperCase() +
+                  player.ratings[0].role.slice(1)}
+              </h3>
+            </span>
+          </div>
+          {}
         </div>
+        {/* 
+          player.competitiveStats.topHeroes = {
+            // [heroName] is the object keys
+            [heroName]: {
+              
+            }
+          }
+        */}
+        {/* Loop through player.competitiveStats and display all content for each heroName */}
+        {/* combat, game, and matchAwards should be in separate divs */}
       </div>
     </>
   )
